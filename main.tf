4bd5e17320a116eeebcfc94110b4d49d318e3fa7
Parthiban <36194811+myapps-solution@users.noreply.github.com>
2026-03-16 19:56:21 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "ec2_private_instance" {
-  source        = "./modules/ec2_private_instance"
-  instance_type = var.instance_type
-  ami_id        = var.ami_id
-  subnet_id     = var.subnet_id
-  key_name      = var.key_name
+  source    = "./modules/ec2_private_instance"
+  ami_id    = var.ami_id
+  subnet_id = var.subnet_id
+  key_name  = var.key_name
 }
