8cb4e675b8dc373996a767ffe3139bd136e5e3e3
Parthiban <36194811+myapps-solution@users.noreply.github.com>
2026-03-16 18:21:00 +0530

    Created variables.tf

---
+++ modules/ec2_private_instance/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
